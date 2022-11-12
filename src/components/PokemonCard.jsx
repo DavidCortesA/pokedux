48ead2f36ed7b704a176ce71e409a9f7c875695d
David Cortez <david.cortes.ayala@hotmail.com>
2022-11-12 15:35:41 -0600

    Pagina final

---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,31 @@
+import { Card } from 'antd';
+import Meta from 'antd/lib/card/Meta';
+import { useDispatch } from 'react-redux';
+import { setFavorite } from '../actions';
+import './PokemonList.css';
+import StarButton from './StarButton';
+
+const PokemonCard = ({ name, image, types, id, favorite }) => {
+  const dispatch = useDispatch();
+  const typesString = types.map(elem => elem.type.name).join(', ');
+
+  const handleOnFavorite = () => {
+    dispatch(setFavorite({pokemonId: id}))
+  }
+  return (
+    <Card
+      title={name}
+      cover={
+        <img
+          src={image}
+          alt={name}
+        />
+      }
+      extra={<StarButton isFavorite={favorite} onClick={handleOnFavorite} />}
+    >
+      <Meta description={typesString} />
+    </Card>
+  );
+};
+
+export default PokemonCard;
